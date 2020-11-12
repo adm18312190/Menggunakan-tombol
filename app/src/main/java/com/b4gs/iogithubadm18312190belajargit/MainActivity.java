234219a5e73219c7ca53cfdc2f176b53ddfdd39f
adm18312190 <adm.18312190@gmail.com>
2020-11-12 16:02:14 +0700

    commit 1

---
+++ app/src/main/java/com/b4gs/iogithubadm18312190belajargit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.b4gs.iogithubadm18312190belajargit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
